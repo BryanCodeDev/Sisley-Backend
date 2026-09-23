089ada7298dcd276f5870d99028b846a39e936ce
BryanCodeDev <tiagotroller021019@gmail.com>
2026-09-23 18:50:11 -0500

    feat: Actualizacion de nombre

--- src/utils/migrate.js
+++ src/utils/migrate.js
@@ -3,9 +3,12 @@ import fs from 'fs'
 import path from 'path'
 
 const migrations = [
+  `SET FOREIGN_KEY_CHECKS = 0`,
+
   `DROP TABLE IF EXISTS reviews`,
   `DROP TABLE IF EXISTS order_items`,
   `DROP TABLE IF EXISTS order_status_history`,
+  `DROP TABLE IF EXISTS payment_webhooks`,
   `DROP TABLE IF EXISTS payment_transactions`,
   `DROP TABLE IF EXISTS payments`,
   `DROP TABLE IF EXISTS cart_items`,
@@ -17,6 +20,8 @@ const migrations = [
   `DROP TABLE IF EXISTS users`,
   `DROP TABLE IF EXISTS roles`,
   `DROP TABLE IF EXISTS settings`,
+
+  `SET FOREIGN_KEY_CHECKS = 1`,
 
   `CREATE TABLE IF NOT EXISTS roles (
     id INT AUTO_INCREMENT PRIMARY KEY,
