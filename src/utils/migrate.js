bb8939e1474f2e5357daf34e7928862615002ba9
BryanCodeDev <tiagotroller021019@gmail.com>
2026-09-23 18:46:08 -0500

    feat: Actualizacion de nombre

--- src/utils/migrate.js
+++ src/utils/migrate.js
@@ -6,6 +6,7 @@ const migrations = [
   `DROP TABLE IF EXISTS reviews`,
   `DROP TABLE IF EXISTS order_items`,
   `DROP TABLE IF EXISTS order_status_history`,
+  `DROP TABLE IF EXISTS payment_transactions`,
   `DROP TABLE IF EXISTS payments`,
   `DROP TABLE IF EXISTS cart_items`,
   `DROP TABLE IF EXISTS product_images`,
